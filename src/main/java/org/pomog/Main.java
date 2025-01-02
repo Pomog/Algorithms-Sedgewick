e30664786b8274474c77d7496243c1a65741a61a
Yurii Panasuik <thoryur@gmail.com>
2025-01-02 22:40:52 +0200

    BinarySearch

--- src/main/java/org/pomog/Main.java
+++ src/main/java/org/pomog/Main.java
@@ -1,7 +1,13 @@
 package org.pomog;
 
+import org.pomog.std.StdOut;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        System.out.println(System.getProperty("java.class.path"));
+        
+        StdOut.println("Hello, stdlib!");
+        
     }
 }
