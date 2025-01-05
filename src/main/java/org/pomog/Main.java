5adf332bf376b8265e2710b30e83c9cf35053bf8
Yurii Panasuik <thoryur@gmail.com>
2025-01-06 00:52:54 +0200

    executeStaticMethod

--- src/main/java/org/pomog/Main.java
+++ src/main/java/org/pomog/Main.java
@@ -8,8 +8,18 @@ public class Main {
         System.out.println(System.getProperty("java.class.path"));
         
         StdOut.println("Hello, stdlib!");
+        
+        executeStaticMethod(Main::staticMethod);
        
 
         
+    }
+    
+    public static void staticMethod() {
+        StdOut.println("Static method executed!");
+    }
+    
+    public static void executeStaticMethod(Runnable method) {
+        method.run();
     }
 }
